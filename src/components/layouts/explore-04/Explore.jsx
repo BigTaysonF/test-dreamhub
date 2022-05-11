325fc5ea005e3ce3a2d02232ba867bb43789e670
BigTaysonF <104858814+BigTaysonF@users.noreply.github.com>
2022-05-11 16:16:53 -0500

    changes

--- src/components/layouts/explore-04/Explore.jsx
+++ src/components/layouts/explore-04/Explore.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React , { useRef } from 'react';
+import { Link  } from "react-router-dom";
 import { Accordion } from 'react-bootstrap-accordion'
 import ExploreItem from './ExploreItem';
 import todayPickData from '../../../assets/fake-data/data-today-pick';
 
 const Explore = props => {
     const data = props.data;
+
+    const btnSearch = () => {
+        btnSearch.current.classList.toggle('active');
+    }
+
     return (
         <section className="tf-explore tf-section">
             <div className="tayson" />
@@ -12,6 +18,14 @@ const Explore = props => {
                 <div className="row">
                     <div className="col-xl-3 col-lg-3 col-md-12">
                         <div id="side-bar" className="side-bar style-3">
+                                    <div className="header-search1 flat-show-search" id="s1">
+                                            <form action="#" method="get" role="search" className="search-form">
+                                                <input type="search" id="s" className="search-field" placeholder="Search..." name="s" title="Search for" required="" />
+                                                <button className="search search-submit" type="submit" title="Search">
+                                                    <i className="icon-fl-search-filled"></i>
+                                                </button>
+                                            </form>
+                                    </div>
                             {
                                 data.map((item,index) => (
                                     <div className="widget widget-category mgbt-24 boder-bt" key={index}>
