c47d4689a8e2c22495f47365114e00b98f7ee73f
BigTaysonF <104858814+BigTaysonF@users.noreply.github.com>
2022-05-17 17:45:41 -0500

    changes

--- src/components/header/DarkMode.jsx
+++ src/components/header/DarkMode.jsx
@@ -32,12 +32,11 @@ const DarkMode = () => {
     }
     return (
         <div className="mode_switcher">
-        <h6>Dark mode <strong>Available</strong></h6>
+       
         <Link to="#" 
             onClick={e => switchTheme(e)}  >
         </Link>
-
-    </div>
+        </div>
     );
 }
 
